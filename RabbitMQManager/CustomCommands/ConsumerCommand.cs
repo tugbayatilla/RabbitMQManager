5e41a028c46fa771c7ce4ae2097538a93e37a3b4
Tugbay Atilla <tatilla.ext@igus.net>
2019-04-18 17:45:22 +0200

    ConsumerCommand dynamicPropertName enhanced

--- RabbitMQManager/CustomCommands/ConsumerCommand.cs
+++ RabbitMQManager/CustomCommands/ConsumerCommand.cs
@@ -18,7 +18,7 @@ namespace RabbitMQManager.CustomCommands
             this.RequiredArguments.Add(new RequiredCommandArgument() { Name = nameof(QueueName), Value = null });
 
             this.OptionalArguments.Add(new OptionalCommandArgument() { Name = nameof(OutputFolder), Value = "", Description = "if defined, output will be recorded into the folder" });
-            this.OptionalArguments.Add(new OptionalCommandArgument() { Name = nameof(MessageType), Value = "none", Description = "ax, sales, factory, d365, none" });
+            this.OptionalArguments.Add(new OptionalCommandArgument() { Name = nameof(DynamicPropertyName), Value = "", Description = "give the name of the property, use dot to find the path" });
             this.OptionalArguments.Add(new OptionalCommandArgument() { Name = nameof(ExchangeType), Value = "direct" });
             this.OptionalArguments.Add(new OptionalCommandArgument() { Name = nameof(OutputFileNameFormat), Value = "SO_{0}.txt" });
             this.OptionalArguments.Add(new OptionalCommandArgument() { Name = nameof(Exchange), Value = null, Description = "name of the exchange" });
@@ -36,14 +36,14 @@ namespace RabbitMQManager.CustomCommands
 
         //optionals
         internal string OutputFolder => this.OptionalArguments.First(p => p.Name == nameof(OutputFolder)).GetValueAsStringIfExist();
-        internal string MessageType => this.OptionalArguments.First(p => p.Name == nameof(MessageType)).GetValueAsStringIfExist();
+        internal string DynamicPropertyName => this.OptionalArguments.First(p => p.Name == nameof(DynamicPropertyName)).GetValueAsStringIfExist();
         internal string ExchangeType => this.OptionalArguments.First(p => p.Name == nameof(ExchangeType)).GetValueAsStringIfExist();
         internal string OutputFileNameFormat => this.OptionalArguments.First(p => p.Name == nameof(OutputFileNameFormat)).GetValueAsStringIfExist();
         internal string Exchange => this.OptionalArguments.First(p => p.Name == nameof(Exchange)).GetValueAsStringIfExist();
         internal string RoutingKey => this.OptionalArguments.First(p => p.Name == nameof(RoutingKey)).GetValueAsStringIfExist();
         internal string DeadLetterExchange => this.OptionalArguments.First(p => p.Name == nameof(DeadLetterExchange)).GetValueAsStringIfExist();
         internal Boolean ShowHeaders => this.OptionalArguments.First(p => p.Name == nameof(ShowHeaders)).IsUsed;
-        internal Boolean NoAutoAck  => this.OptionalArguments.First(p => p.Name == nameof(NoAutoAck)).IsUsed;
+        internal Boolean NoAutoAck => this.OptionalArguments.First(p => p.Name == nameof(NoAutoAck)).IsUsed;
 
         protected override void ExecuteCommand()
         {
@@ -59,34 +59,27 @@ namespace RabbitMQManager.CustomCommands
                 {
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body);
-                    var uniqueId = Guid.NewGuid().ToString();
+                    dynamic uniqueId = String.Empty;
 
                     dynamic entity = Newtonsoft.Json.JsonConvert.DeserializeObject<ExpandoObject>(message);
                     if (entity != null)
                     {
-                        var entityDictionary = (IDictionary<string, object>)entity;
-                        uniqueId = entity[MessageType];
+                        try
+                        {
+                            if (!String.IsNullOrEmpty(DynamicPropertyName))
+                            {
+                                foreach (var item in DynamicPropertyName.Split(".".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                                {
+                                    entity = ((IDictionary<string, object>)entity)[item];
+                                }
+                                uniqueId = entity;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception($"ReceivedMessage: {message}", ex);
+                        }
                     }
-                    //switch (MessageType)
-                    //{
-                    //    case "ax":
-                    //        orderNumber = entity.SalesOrderHeader_SALESID;
-                    //        break;
-                    //    case "sales":
-                    //        orderNumber = entity.Order.Id;
-                    //        break;
-                    //    case "factory":
-                    //        eventTimestamp = entity.FactoryOrder.EventTimestamp;
-                    //        orderNumber = entity.FactoryOrder.Id;
-                    //        break;
-                    //    case "d365":
-                    //        orderNumber = entity.foreignId;
-                    //        break;
-                    //    default:
-                    //        break;
-                    //}
-                    
-
 
                     if (!String.IsNullOrEmpty(OutputFolder))
                     {
@@ -95,7 +88,7 @@ namespace RabbitMQManager.CustomCommands
                         var filePath = Path.Combine(OutputFolder, fileName);
                         FileManager.CreateFileWithData(filePath, message);
                     }
-                    Notification.Notify($"{uniqueId}");
+                    Notification.Notify($"Done:{DynamicPropertyName}:{uniqueId}");
                     if (ShowHeaders)
                     {
                         foreach (var item in ea.BasicProperties.Headers)
